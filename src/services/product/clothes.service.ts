38194d4def56e01d2ed88ed7e92b2a1cc80c846f
phamvant <pham.t.286.01@gmail.com>
2024-03-09 21:37:58 +0900

    add launch file to debug and rest client token variable

--- src/services/product/clothes.service.ts
+++ src/services/product/clothes.service.ts
@@ -1,26 +1,6 @@
 import { Product } from "./product.index.service";
 
 export class Clothes extends Product {
-  constructor({
-    product_name,
-    product_thumbs,
-    product_description,
-    product_price,
-    product_quantity,
-    product_variations,
-    product_shop,
-  }: any) {
-    super({
-      product_name,
-      product_thumbs,
-      product_description,
-      product_price,
-      product_quantity,
-      product_variations,
-      product_shop,
-    });
-  }
-
   async createProduct() {
     const newProductName = await super.createProduct(1);
     return newProductName;
