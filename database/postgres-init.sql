393086a86e1a8a22add18daafcbede580f598528
phamvant <pham.t.286.01@gmail.com>
2024-04-04 01:56:48 +0900

    updatechangelog sql

--- database/postgres-init.sql
+++ database/postgres-init.sql
@@ -1,9 +1,9 @@
-CREATE DATABASE "dev" WITH OWNER "thuan";
-GRANT ALL PRIVILEGES ON DATABASE "dev" TO "thuan";
--- \ connect "dev";
-CREATE SCHEMA app_schema AUTHORIZATION "thuan";
-GRANT CREATE,
-  USAGE ON SCHEMA app_schema TO "thuan";
-GRANT USAGE,
-  SELECT ON ALL SEQUENCES IN SCHEMA app_schema to "thuan";
-GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA app_schema to "thuan";
+-- CREATE DATABASE "dev" WITH OWNER "thuan";
+-- GRANT ALL PRIVILEGES ON DATABASE "dev" TO "thuan";
+-- -- \ connect "dev";
+-- CREATE SCHEMA app_schema AUTHORIZATION "thuan";
+-- GRANT CREATE,
+--   USAGE ON SCHEMA app_schema TO "thuan";
+-- GRANT USAGE,
+--   SELECT ON ALL SEQUENCES IN SCHEMA app_schema to "thuan";
+-- GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA app_schema to "thuan";
